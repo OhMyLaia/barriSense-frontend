fdb5fdd70e1d0892aea10727d97247f543280683
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-15 17:11:44 +0200

    added hook in Home to test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
 import MapPage from "../components/MapPage";
+import { useFeedbacks } from "../hooks/useFeedBacks";
 
 function HomePage() {
+  const { feedbacks, setFeedbacks } = useFeedbacks();
+  console.log(feedbacks);
   return (
     <div>
       <MapPage />
