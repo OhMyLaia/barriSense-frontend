206881c54027edb6b7082ea54d7cf7d0702f79cf
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-15 18:34:43 +0200

    green for few feedbacks

--- src/components/map/helpers/getColorByFeedBack.js
+++ src/components/map/helpers/getColorByFeedBack.js
@@ -1,5 +1,5 @@
 export const getColorByFeedback = (feedbacks) => {
-  if (feedbacks === 0) return "#FFFFFF"; // gray-300
+  if (feedbacks === 0) return "#22C55E"; // gray-300
   if (feedbacks <= 2) return "#000000"; // yellow-200
   if (feedbacks <= 4) return "#FBBF24"; // yellow-4001765dbff
   if (feedbacks <= 6) return "#F97316"; // orange-500
