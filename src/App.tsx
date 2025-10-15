bc85818e140c1337352c6d78898f233d9eb88889
OhMyLaia <ohmylaia@live.com>
2025-10-15 14:42:23 +0200

    WIP: navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/Homepage";
+import FeedbackPage from "./pages/FeedbackPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  return <div className="bg-red-500">hola</div>;
+  return <>
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/queixes" element={<FeedbackPage />} />
+        {/* Catch-all route */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+
+  </BrowserRouter>
+  </>;
 }
 
 export default App;
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,44 @@
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div>Navbar</div>
-    )
+<header className="bg-gradient-to-r from-blue-400 to-slate-900 text-slate-200 shadow-md w-full overflow-x-hidden">
+            <div className="container mx-auto px-2 py-5 flex items-center justify-between ">
+                {/* Logo */}
+                <p className="text-xl font-bold tracking-wide">barriSense</p>
+
+                {/* Navigation */}
+                <NavigationMenu>
+                    <NavigationMenuList className="gap-2 bg-transparent w-[10%]">
+                        <NavigationMenuItem >
+                            <NavigationMenuLink
+                                asChild
+                                className={`${navigationMenuTriggerStyle()} text-white hover:text-gray-300 transition-colors bg-transparent`}
+                            >
+                                <Link to="/">Home</Link>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink
+                                asChild
+                                className={`${navigationMenuTriggerStyle()} text-white hover:text-gray-300 transition-colors bg-transparent`}
+                            >
+                                <Link to="/queixes">Queixa</Link>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+        </header>
+    );
 }
 
-export default Navbar
+export default Navbar;
